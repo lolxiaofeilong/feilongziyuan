5877431e248900544bb1a63930e7a2cef81f6685
e <651956368@qq.com>
2020-12-25 17:17:28 +0800

    更改之后的一次提交

---
+++ pages/firstShow.vue
@@ -0,0 +1,56 @@
+<template>
+	<view>
+		<!-- <button type="default" @click="goMainPage">去主页</button> -->
+		<view class="ad" >
+			<image src="http://139.155.90.219:1000/lanch.png" mode="widthFix"></image>
+			<view class="tiaoguo" @click="goMainPage">跳过</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			goMainPage(){
+				console.log(111111111111)
+				uni.switchTab({
+				    url: './tabbar/tabbar-1/tabbar-1'
+				});
+			}
+		}
+	}
+</script>
+
+<style scoped>
+	page{
+		width: 100%;
+		height: 100%;
+		/* background-color: red; */
+	}
+	.ad{
+		width: 100%;
+		height: 100%;
+		position: relative;
+	}
+	.ad image{
+		width: 100%;
+		height: 100%;
+	}
+	.tiaoguo{
+		width: 40px;
+		height: 40px;
+		background-color: #999;
+		position: absolute;
+		right: 10px;
+		top:20px;
+		text-align: center;
+		line-height: 40px;
+		border-radius: 50%;
+		color: #fff;
+	}
+</style>
